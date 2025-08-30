4caeef3fd281610ca9fb8621fbd1d849187ef0f9
Martin Wainaina <122657979+codexoft-ke@users.noreply.github.com>
2025-08-30 10:10:59 +0000

    Fix import paths in schema.py to use absolute references for models

--- crm/schema.py
+++ crm/schema.py
@@ -2,7 +2,7 @@ import graphene
 from graphene_django import DjangoObjectType
 from graphene_django.filter import DjangoFilterConnectionField
 from graphene import relay
-from .models import Customer, Product, Order
+from crm.models import Customer, Product, Order
 from .filters import CustomerFilter, ProductFilter, OrderFilter
 from django.core.exceptions import ValidationError
 from django.db import transaction
